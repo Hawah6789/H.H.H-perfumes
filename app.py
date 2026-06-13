5cf8591c88fdbae7a95c7c8d0248019137e8c6e0
Hawah6789 <wnkbyyzp5b@privaterelay.appleid.com>
2026-06-13 13:33:28 +0300

    fix app.py

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>H.H.H Perfumes</h1>
-    <h2>Luxury Fragrances</h2>
-
-    <p>Welcome to H.H.H Perfumes.</p>
-
-    <h3>Our Collection</h3>
-
-    <ul>
-        <li>Oud Royal</li>
-        <li>Rose Elegance</li>
-        <li>Midnight Amber</li>
-    </ul>
-
-    <button>Shop Now</button>
-    """
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
